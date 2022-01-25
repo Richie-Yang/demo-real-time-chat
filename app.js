3c57163719ac96aef36478f7996ec56e1fd291bd
Richie-Yang <richieyang8813@gmail.com>
2022-01-25 10:27:46 +0800

    feat: project init

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const { engine } = require('express-handlebars')
+
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
+const app = express()
+const PORT = process.env.PORT
+
+app.engine('hbs', engine({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
+
+app.get('/', (req, res) => {
+  return res.render('index')
+})
+
+app.listen(PORT, () => {
+  console.info(`Express server is listening at 127.0.0.1:${PORT}`)
+})
---
+++ config/mongoose.js
@@ -0,0 +1,2 @@
+const mongoose = require('mongoose')
+const MONGODB_URI = process.env.MONGODB_URI
